88d238817c55ad9711393f927173affa74175ad4
StivenDelgado <delgadostiven123@gmail.com>
2025-02-07 02:23:11 -0500

    visual creado con captura de datos

--- src/presentation/components/ui/Input.jsx
+++ src/presentation/components/ui/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const Input = ({type, placeholder}) => {
+export const Input = ({type, placeholder, name}) => {
   return (
-    <input type={type} class='w-full block border-solid border-[2px] border-Cereza rounded-full px-[1rem] py-[0.5rem] mt-1 mb-4 ' placeholder={placeholder}/>
+    <input type={type} name={name} class='w-full block border-solid border-[2px] border-Cereza rounded-full px-[1rem] py-[0.5rem] mt-1 mb-4 ' placeholder={placeholder}/>
   )
 }
