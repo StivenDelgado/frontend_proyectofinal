43cd9baa457020f8ecbcdd01329d181d39ef7421
Stiven <grstiven1004@gmail.com>
2025-02-07 03:06:14 -0500

    minor changes on register paddings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router";
 import { Home } from "./presentation/view/Home";
 import { Login } from "./presentation/view/Login";
+import Register from "./presentation/view/Register";
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   )
