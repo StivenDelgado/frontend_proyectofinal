7c62e8029f5e3d78332d5aeb00dd53871c09af80
StivenGonzalezz <grstiven1004@gmail.com>
2025-02-07 23:32:57 -0500

    passwordRecovery view create

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import { Home } from "./presentation/view/Home";
 import { Login } from "./presentation/view/Login";
 import Register from "./presentation/view/Register";
+import PasswordRecovery from "./presentation/view/PasswordRecovery";
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/recovery" element={<PasswordRecovery />}/>
       </Routes>
     </>
   )
---
+++ src/presentation/view/PasswordRecovery.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PasswordRecovery = () => {
+  return (
+    <div class='bg-Hueso w-full h-screen flex-col items-center justify-center'>
+      
+      test
+    </div>
+  )
+}
+
+export default PasswordRecovery
