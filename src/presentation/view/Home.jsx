ad610eea738a4a615a12b7d0d05c7f31c2412481
adalinee <lollanuwu@gmail.com>
2025-02-11 23:32:26 -0500

    finalizacion vista Home,rutas con direccion finalizada

--- src/presentation/view/Home.jsx
+++ src/presentation/view/Home.jsx
@@ -5,7 +5,7 @@ import { HomeCard } from "../components/ui/HomeCard";
 import quackCash from "../../assets/img/QuackCash.png"
 import ahorro from "../../assets/img/Ahorro.png";
 import metas from "../../assets/img/Metas.png";
-
+import { Link } from "react-router";
 export const Home = () => {
 
   return (
@@ -19,10 +19,20 @@ export const Home = () => {
           <FaRegBell class="text-Hueso text-[2em]" />
         </header>
 
-        <div class="h-full px-15"> 
+        <div class=" px-15"> 
+
+          <Link to={"/chat-bot"}>
           <HomeCard img={quackCash} tittle="QuackCash" description="¡Ahorra sin esfuerzo! Chatea con nuestro boty recibe planes de ahorro personalizados según tus hábitos. 📈💡"/>
+          </Link>
+
+          <Link to={"/expenses"}>
           <HomeCard img={ahorro} tittle="Control de Gastos" description="Registra, clasifica y mantén a raya tus gastos. Recibe alertas antes de pasarte del límite. ¡Toma el control! 💰🔥"/>
+          </Link>
+
+          <Link to={"/metas"}>
           <HomeCard img={metas} tittle="Mis Metas" description="Fija tus objetivos de ahorro y mira cómo crece tu dinero. ¡Organízate y alcanza tus sueños más rápido! 🚀💵"/>
+          </Link>
+
         </div>
       </div>
       <footer class="w-full h-10 bg-Cereza">
