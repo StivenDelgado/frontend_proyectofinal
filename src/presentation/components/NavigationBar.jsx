053963f98183ba610a5fd3533a2f7d87339eaf6d
Stiven Delgado <delgadostiven123@gmail.com>
2025-02-13 15:24:03 -0500

    arreglando navbar y header

--- src/presentation/components/NavigationBar.jsx
+++ src/presentation/components/NavigationBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { GiDuck, GiReceiveMoney, GiStairsGoal } from 'react-icons/gi';
+import { GiReceiveMoney, GiStairsGoal } from 'react-icons/gi';
 import { IoHomeOutline } from "react-icons/io5";
+import duck from '../../assets/img/duuckOutLine.png'
+
 
 export const NavigationBar = ({indexMenu}) => {
   const options = [
     { name: 'Home', icon: <IoHomeOutline />  },
-    { name: 'Chat', icon: <GiDuck />},
+    { name: 'Chat', icon: <img src={duck} alt="" className='w-5 h-5' /> },
     { name: 'Saveup', icon: <GiReceiveMoney />},
     { name: 'Goals', icon: <GiStairsGoal />}
   ]
@@ -13,7 +15,7 @@ export const NavigationBar = ({indexMenu}) => {
     <div className='border-4 border-CerezaSuave w-[80%] m-auto rounded-3xl h-14 flex flex-row items-center justify-around'>
         {
           options.map((option, index) => (
-            <div key={index} className={`${indexMenu === index ? "bg-CerezaSuave text-white" : "" } flex flex-row items-center space-x-2 p-3 rounded-2xl w-auto text-xl`}>
+            <div key={index} className={`${indexMenu === index ? "border-2 border-CerezaSuave " : "" } flex flex-row items-center space-x-2 p-2 rounded-2xl w-auto text-xl`}>
               {option.icon}
             </div>
           ))
