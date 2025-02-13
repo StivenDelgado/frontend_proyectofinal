c6a595e167615fcbed2b97fd18236eead92f540e
Stiven Delgado <delgadostiven123@gmail.com>
2025-02-12 21:20:26 -0500

    arreglos

--- src/presentation/components/ui/HomeCard.jsx
+++ src/presentation/components/ui/HomeCard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { Link } from "react-router";
 
-export const HomeCard = ({img, tittle, description}) => {
+export const HomeCard = ({img, tittle, description, url}) => {
   
   return (
-    <div class="text-Granito flex pt-10">
+    <Link to={url} class="text-Granito flex pt-10">
       <div class="flex flex-col w-100">
-        <img class="w-25 h-30 " src={img}/>
+        <img class="w-25 h-25" src={img}/>
         <p class="text-center">{tittle} </p>
       </div>
       <p class="flex flex-col text-center justify-center">
         {description}
       </p>
-    </div>
+    </Link>
   );
 };
--- src/presentation/view/Home.jsx
+++ src/presentation/view/Home.jsx
@@ -5,37 +5,28 @@ import { HomeCard } from "../components/ui/HomeCard";
 import quackCash from "../../assets/img/QuackCash.png"
 import ahorro from "../../assets/img/Ahorro.png";
 import metas from "../../assets/img/Metas.png";
-import { Link } from "react-router";
 export const Home = () => {
 
   return (
-    <div class="bg-Hueso w-full h-screen flex flex-col">
-      <div class="m-autow-full h-screen flex-col items-center justify-center">
-        <header class="bg-Cereza px-12 w-full h-50  items-center flex">
-          <FaRegCircleUser class="text-Hueso text-[2em] " />
-          <h1 class="text-Hueso text-[2em] font-bold w-full text-center">
+    <div className="bg-Hueso w-full h-screen flex flex-col">
+      <div className=" h-screen flex-col items-center justify-center">
+        <header className="bg-CerezaSuave px-12 w-full h-50  items-center flex">
+          <FaRegCircleUser className="text-Hueso text-[2em] " />
+          <h1 className="text-Hueso text-[2em] font-bold w-full text-center">
             SaveUp
           </h1>
-          <FaRegBell class="text-Hueso text-[2em]" />
+          <FaRegBell className="text-Hueso text-[2em]" />
         </header>
 
-        <div class=" px-15"> 
+        <div className="w-[75%] m-auto">    
+          <HomeCard url={"/chatbot"} img={quackCash} tittle="QuackCash" description="¡Ahorra sin esfuerzo! Chatea con nuestro boty recibe planes de ahorro personalizados según tus hábitos. 📈💡"/>
 
-          <Link to={"/chatbot"}>
-          <HomeCard img={quackCash} tittle="QuackCash" description="¡Ahorra sin esfuerzo! Chatea con nuestro boty recibe planes de ahorro personalizados según tus hábitos. 📈💡"/>
-          </Link>
+          <HomeCard url={"/expenses"} img={ahorro} tittle="Control de Gastos" description="Registra, clasifica y mantén a raya tus gastos. Recibe alertas antes de pasarte del límite. ¡Toma el control! 💰🔥"/>
 
-          <Link to={"/expenses"}>
-          <HomeCard img={ahorro} tittle="Control de Gastos" description="Registra, clasifica y mantén a raya tus gastos. Recibe alertas antes de pasarte del límite. ¡Toma el control! 💰🔥"/>
-          </Link>
-
-          <Link to={"/goals"}>
-          <HomeCard img={metas} tittle="Mis Metas" description="Fija tus objetivos de ahorro y mira cómo crece tu dinero. ¡Organízate y alcanza tus sueños más rápido! 🚀💵"/>
-          </Link>
-
+          <HomeCard url={"/goals"} img={metas} tittle="Mis Metas" description="Fija tus objetivos de ahorro y mira cómo crece tu dinero. ¡Organízate y alcanza tus sueños más rápido! 🚀💵"/>
         </div>
       </div>
-      <footer class="w-full h-10 bg-Cereza">
+      <footer className="w-full h-10 bg-CerezaSuave">
 
       </footer>
     </div>
